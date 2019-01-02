af678b658127a56bdf30a54e3caf577d05563144
LogeshSanjay <logeshsanjay914@gmail.com>
2019-01-02 20:43:10 +0530

    Java sports

---
+++ logesh/Sports.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Sports
+{
+	public static void main (string args[])
+	{
+		int age;
+		string name,email,country,password;
+		string resume;
+		string hobbies,gender;
+		string city,address;
+	}
+}
